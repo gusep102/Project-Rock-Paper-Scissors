6e47319bbc10e136292d403f32737a50f3c58e0e
Aki <43755568+gperalta102@users.noreply.github.com>
2023-08-16 17:48:01 -0700

    Create Game function consisting of 5 rounds. whoever wins most out of 5 rounds win.
    game function loops 5 times keeping track of who won with counters and printing the final score

--- javascript/script.js
+++ javascript/script.js
@@ -28,26 +28,26 @@ function playRound(playerSelection, computerSelection) {
     // scissors beats paper
     if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors') {
         if (playerSelection == 'rock' && computerSelection == 'scissors') {
-            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-            computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+            // playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+            // computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
             return `You Win! ${playerSelection} beats ${computerSelection}`;
     
         } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-            computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+            // playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+            // computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
             return `You Win! ${playerSelection} beats ${computerSelection}`;
     
         } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-            computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+            // playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+            // computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
             return `You Win! ${playerSelection} beats ${computerSelection}`;
         } else if ( playerSelection == computerSelection){
-            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-            computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+            // playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+            // computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
             return `Tie! ${playerSelection} is the same as ${computerSelection}`
         } else {
-            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-            computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+            // playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+            // computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
             return `You Lose! ${computerSelection} beats ${playerSelection}`;
         }
     } else {
@@ -57,11 +57,22 @@ function playRound(playerSelection, computerSelection) {
     
   }
 
+// calculate final score and winner of the match
+function whoWon(computerWinCounter, playerWinCounter) {
+    if (computerWinCounter == playerWinCounter) {
+        console.log(`Its a tie!`);
+    } else if (computerWinCounter > playerWinCounter) {
+        console.log(`Computer Wins!`);
+    } else {
+        console.log(`Player Wins!`);
+    }
+    
+}
 //   play 5 rounds of the game while keeping track of who wins best out of 5
 function game(){
     //keep track of wins
     let computerWinCounter = 0;
-    let playerWinCounter =0;
+    let playerWinCounter = 0;
 
     for (let index = 0; index < 5; index++) {
     // get the input from the user
@@ -69,10 +80,26 @@ function game(){
     // get the computer input
     let computerSelection = getComputerChoice();
     // play a round
-    console.log(playRound(playerSelection, computerSelection));
+    let winner = playRound(playerSelection, computerSelection);
     // report winner
 
+    console.log(winner);
+
+    if (winner == `Tie! ${playerSelection} is the same as ${computerSelection}`) {
+        computerWinCounter++;
+        playerWinCounter++;
+        console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+    } else if (winner == `You Win! ${playerSelection} beats ${computerSelection}`) {
+        playerWinCounter++;
+        console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+    } else {
+        computerWinCounter++;
+        console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
     }
+
+    }
+
+    whoWon(computerWinCounter, playerWinCounter);
 }
 
 
