b9ad8e0a0bbf035e9fed91e3a838d2f2022c09e5
Aki <43755568+gperalta102@users.noreply.github.com>
2023-08-16 17:24:17 -0700

    Create basic HTML file with javascript file externally linked
    The javascript file does the following
    
    - generates a computer choice of rock paper scissors through random number generated between 1-3 (1 = rock, 2 = paper, 3 = scissors)
    - function to play a round of rock paper scissor
    -- lowercases both inputs
    -- verifies player input is rock paper or scissors else prints invalid input
    -- once verified logic of the game checks the following for (rock > scissors, scissors > paper, paper > rock) else player loses

---
+++ javascript/script.js
@@ -0,0 +1,84 @@
+function getComputerChoice() {
+    // select a random number between 1 and 3
+    let compSelection = Math.floor(Math.random() * 3) + 1;
+    // 1 is rock
+    // 2 is paper
+    // 3 is scissors
+    if (compSelection === 1) {
+        return 'rock';
+    } else if (compSelection === 2) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+    // return the choice to the function
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    // lowercase playerSelection for simpler comparison
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    // console.log(playerSelection);
+    // console.log(computerSelection);
+
+    // compare playerSelection and computerSelection
+    // rock beats scissors
+    // paper beats rock
+    // scissors beats paper
+    if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors') {
+        if (playerSelection == 'rock' && computerSelection == 'scissors') {
+            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+            computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+    
+        } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+            computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+    
+        } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+            computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        } else if ( playerSelection == computerSelection){
+            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+            computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+            return `Tie! ${playerSelection} is the same as ${computerSelection}`
+        } else {
+            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+            computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        }
+    } else {
+        return 'invalid input';
+    }
+
+    
+  }
+
+//   play 5 rounds of the game while keeping track of who wins best out of 5
+function game(){
+    //keep track of wins
+    let computerWinCounter = 0;
+    let playerWinCounter =0;
+
+    for (let index = 0; index < 5; index++) {
+    // get the input from the user
+    let playerSelection = prompt("Rock, Paper, or Scissors?",'');
+    // get the computer input
+    let computerSelection = getComputerChoice();
+    // play a round
+    console.log(playRound(playerSelection, computerSelection));
+    // report winner
+
+    }
+}
+
+
+// main
+// console.log(computerHand);
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+game();
+// console.log(playRound(playerSelection, computerSelection));
