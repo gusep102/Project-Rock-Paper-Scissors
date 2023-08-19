1ff7f7604b13d7fac6ba618eb1c6e14e15bca4e2
Aki <43755568+gperalta102@users.noreply.github.com>
2023-08-18 17:09:35 -0700

    Create UI to select throw
    keeps track of who won and ends game when someone wins 5 rounds

--- javascript/script.js
+++ javascript/script.js
@@ -69,43 +69,165 @@ function whoWon(computerWinCounter, playerWinCounter) {
     
 }
 //   play 5 rounds of the game while keeping track of who wins best out of 5
-function game(){
-    //keep track of wins
-    let computerWinCounter = 0;
-    let playerWinCounter = 0;
+// function game(){
+//     //keep track of wins
+//     let computerWinCounter = 0;
+//     let playerWinCounter = 0;
 
-    for (let index = 0; index < 5; index++) {
-    // get the input from the user
-    let playerSelection = prompt("Rock, Paper, or Scissors?",'');
-    // get the computer input
-    let computerSelection = getComputerChoice();
-    // play a round
+//     for (let index = 0; index < 5; index++) {
+//     // get the input from the user
+//     let playerSelection = prompt("Rock, Paper, or Scissors?",'');
+//     // get the computer input
+//     let computerSelection = getComputerChoice();
+//     // play a round
+//     let winner = playRound(playerSelection, computerSelection);
+//     // report winner
+
+//     console.log(winner);
+
+//     if (winner == `Tie! ${playerSelection} is the same as ${computerSelection}`) {
+//         computerWinCounter++;
+//         playerWinCounter++;
+//         console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+//     } else if (winner == `You Win! ${playerSelection} beats ${computerSelection}`) {
+//         playerWinCounter++;
+//         console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+//     } else {
+//         computerWinCounter++;
+//         console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+//     }
+
+//     }
+
+//     whoWon(computerWinCounter, playerWinCounter);
+// }
+
+
+// Create three buttons, one for each selection.
+const btnRock = document.querySelector('.rock');
+const btnPaper = document.querySelector('.paper');
+const btnScissors = document.querySelector('.scissors');
+
+let computerWinCounter = 0;
+let playerWinCounter = 0;
+
+const container = document.querySelector('.container');
+const currentScoreElement = document.createElement('div');
+const roundInfoElement = document.createElement('div');
+
+currentScoreElement.textContent = `Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`;
+
+container.appendChild(currentScoreElement);
+container.appendChild(roundInfoElement);
+
+
+
+btnRock.addEventListener('click' , () => {
+    playerSelection = 'rock';
+    computerSelection = getComputerChoice();
+
     let winner = playRound(playerSelection, computerSelection);
-    // report winner
-
-    console.log(winner);
+    // console.log(winner);
+    roundInfoElement.textContent = `Round info: `+ winner;
 
     if (winner == `Tie! ${playerSelection} is the same as ${computerSelection}`) {
         computerWinCounter++;
         playerWinCounter++;
-        console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+        currentScoreElement.textContent = `Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`;
     } else if (winner == `You Win! ${playerSelection} beats ${computerSelection}`) {
         playerWinCounter++;
-        console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+        currentScoreElement.textContent = `Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`;
     } else {
         computerWinCounter++;
-        console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+        currentScoreElement.textContent = `Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`;
     }
 
+    if (computerWinCounter == 5){
+        currentScoreElement.textContent = `Computer is winner with ${computerWinCounter} rounds won!`;
+        computerWinCounter = 0;
+        playerWinCounter = 0;
+    }else if (playerWinCounter == 5){
+        currentScoreElement.textContent = `Player is winner with ${playerWinCounter} rounds won!`;
+        computerWinCounter = 0;
+        playerWinCounter = 0;
     }
 
-    whoWon(computerWinCounter, playerWinCounter);
-}
+});
+
+btnPaper.addEventListener('click', () =>{
+    playerSelection = 'paper';
+    computerSelection = getComputerChoice();
+
+    let winner = playRound(playerSelection, computerSelection);
+    // console.log(winner);
+
+    roundInfoElement.textContent = `Round info: `+ winner;
 
 
+    if (winner == `Tie! ${playerSelection} is the same as ${computerSelection}`) {
+        computerWinCounter++;
+        playerWinCounter++;
+        // console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+        currentScoreElement.textContent = `Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`;
+    } else if (winner == `You Win! ${playerSelection} beats ${computerSelection}`) {
+        playerWinCounter++;
+        // console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+        currentScoreElement.textContent = `Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`;
+    } else {
+        computerWinCounter++;
+        // console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+        currentScoreElement.textContent = `Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`;
+    }
+
+    if (computerWinCounter == 5){
+        currentScoreElement.textContent = `Computer is winner with ${computerWinCounter} rounds won!`;
+        computerWinCounter = 0;
+        playerWinCounter = 0;
+    }else if (playerWinCounter == 5){
+        currentScoreElement.textContent = `Player is winner with ${playerWinCounter} rounds won!`;
+        computerWinCounter = 0;
+        playerWinCounter = 0;
+    }
+
+});
+
+btnScissors.addEventListener('click', () => {
+    playerSelection = 'scissors';
+    computerSelection = getComputerChoice();
+
+    let winner = playRound(playerSelection, computerSelection);
+    // console.log(winner);
+        roundInfoElement.textContent = `Round info: `+ winner;
+
+    if (winner == `Tie! ${playerSelection} is the same as ${computerSelection}`) {
+        computerWinCounter++;
+        playerWinCounter++;
+        // console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+        currentScoreElement.textContent = `Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`;
+    } else if (winner == `You Win! ${playerSelection} beats ${computerSelection}`) {
+        playerWinCounter++;
+        // console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+        currentScoreElement.textContent = `Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`;
+    } else {
+        computerWinCounter++;
+        // console.log(`Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`);
+        currentScoreElement.textContent = `Current scores Player: ${playerWinCounter} Computer: ${computerWinCounter}`;
+    }
+
+    if (computerWinCounter == 5){
+        currentScoreElement.textContent = `Computer is winner with ${computerWinCounter} rounds won!`;
+        computerWinCounter = 0;
+        playerWinCounter = 0;
+    }else if (playerWinCounter == 5){
+        currentScoreElement.textContent = `Player is winner with ${playerWinCounter} rounds won!`;
+        computerWinCounter = 0;
+        playerWinCounter = 0;
+    }
+
+});
 // main
 // console.log(computerHand);
 // const playerSelection = "rock";
 // const computerSelection = getComputerChoice();
-game();
+// game();/
 // console.log(playRound(playerSelection, computerSelection));
